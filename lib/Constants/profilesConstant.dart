6fc06ac8f8b56e143c99b4e4b520776c24426819
Deepeshac2890 <deepeshac290@gmail.com>
2022-02-01 12:30:11 +0530

    Deployed on Firebase

--- lib/Constants/profilesConstant.dart
+++ lib/Constants/profilesConstant.dart
@@ -1,9 +1,10 @@
 class Constants {
   static const PROFILE_LINKEDIN =
       'https://www.linkedin.com/in/deepesh-acharyaa/';
-  static const PROFILE_FACEBOOK = 'https://www.facebook.com/';
+  static const PROFILE_FACEBOOK =
+      'https://www.facebook.com/deepesh.acharya.319';
   static const PROFILE_GITHUB = 'https://github.com/Deepeshac2890';
-  static const PROFILE_TWITTER = 'https://twitter.com/';
+  static const PROFILE_TWITTER = 'https://twitter.com/DeepeshAchary13';
   static const PROFILE_INSTAGRAM = 'https://www.instagram.com/deepeshac300/';
   static const PROFILE_MAIL = 'mailto:deepeshac280@gmail.com';
 }
